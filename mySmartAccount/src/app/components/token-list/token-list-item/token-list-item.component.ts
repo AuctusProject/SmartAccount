5ab53ea0ad2ff2201b6c880c59483262bc813710
Daniel Silveira <dansilveira@gmail.com>
2018-05-26 00:47:28 -0300

    Tokens components

---
+++ mySmartAccount/src/app/components/token-list/token-list-item/token-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-token-list-item',
+  templateUrl: './token-list-item.component.html',
+  styleUrls: ['./token-list-item.component.css']
+})
+export class TokenListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ mySmartAccount/src/app/components/token-list/token-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-token-list',
+  templateUrl: './token-list.component.html',
+  styleUrls: ['./token-list.component.css']
+})
+export class TokenListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
