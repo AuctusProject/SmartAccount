3579ee405984b56261014cdb936083097c456e59
Guilherme Gusman <guilherme.gusman@lg7.com.br>
2018-05-25 23:06:12 -0300

    working version without angular material

---
+++ mySmartAccount/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
+
+@NgModule({
+    exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)]
+})
+
+export class AppRoutingModule { }
--- mySmartAccount/src/app/app.module.ts
+++ mySmartAccount/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 import { AppComponent } from './app.component';
@@ -10,9 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
