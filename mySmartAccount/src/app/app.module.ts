0185523389be9b2be6ea426844fdf67909ee9263
Guilherme Gusman <guilherme.gusman@lg7.com.br>
2018-05-25 23:39:50 -0300

    extension panel

--- mySmartAccount/src/app/app.module.ts
+++ mySmartAccount/src/app/app.module.ts
@@ -2,18 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
 import { AppRoutingModule } from './/app-routing.module';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
+import { ExtensionListComponent } from './components/extension-list/extension-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExtensionListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatExpansionModule
   ],
   
   providers: [],
