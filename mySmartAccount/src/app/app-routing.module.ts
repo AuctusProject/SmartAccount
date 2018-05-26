6b74f62ffae1cc56ecc3046e8435bc5f60caad6f
Daniel Silveira <dansilveira@gmail.com>
2018-05-26 02:22:56 -0300

    add token page

--- mySmartAccount/src/app/app-routing.module.ts
+++ mySmartAccount/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTokenComponent } from './components/add-token/add-token.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'add-token', component: AddTokenComponent }
 ];
 
 @NgModule({
